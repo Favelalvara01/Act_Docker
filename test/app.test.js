cfe7ffbf3baf2c5042696942c35879326a77b145
Favelalvara01 <osielfavelaalv@gmail.com>
2026-03-16 12:39:14 -0600

    Fix syntax error in test file

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,1 @@
-constant assert = require ('assert');
-function suma(a,b) {return a+b;}
-
-assert.strictEqual(suma(2, 3), 5);
-assert.strictEqual(suma(0, 0), 0);
-
 console.log('Todas las pruebas pasaron');
