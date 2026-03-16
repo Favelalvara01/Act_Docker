46708f27f4a2a0b46edb7675e8ad2dee9eb611ac
Favelalvara01 <osielfavelaalv@gmail.com>
2026-03-16 13:59:55 -0600

    Update server log message for clarity

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.end('Osiel Alberto Favela Alvarado');
 });
-server.listen(3000, () => console.log('Corriendo en puerto 300011'));
+server.listen(3000, () => console.log('Favela Alvarado Osiel Alberto DSM31 - Corriendo en puerto 3000'));
