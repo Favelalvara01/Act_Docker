2da056c210b22a5aa85502e54d49b475ca80fbea
Favelalvara01 <osielfavelaalv@gmail.com>
2026-03-16 13:20:10 -0600

    Fix syntax for server creation in index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-js
 const http = require('http');
-const server = http.createServer((req, res)) => {
+const server = http.createServer((req, res) => {
   res.end('Hola desde Docker + GitHub Actions!');
 });
 server.listen(3000, () => console.log('Corriendo en puerto 3000'));
