6b09c3425fe3878cb4f02b5be780c236d073dded
Favelalvara01 <osielfavelaalv@gmail.com>
2026-03-16 13:27:24 -0600

    Change response message in server to new text

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end('Hola desde Docker + GitHub Actions!');
+  res.end('Osiel Alberto Favela Alvarado');
 });
 server.listen(3000, () => console.log('Corriendo en puerto 3000'));
