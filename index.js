fa7e697b704e075eefd9e3a3c8872e668e8b1d55
Favelalvara01 <osielfavelaalv@gmail.com>
2026-03-16 13:37:59 -0600

    Update server log message to new port number

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.end('Osiel Alberto Favela Alvarado');
 });
-server.listen(3000, () => console.log('Corriendo en puerto 3000'));
+server.listen(3000, () => console.log('Corriendo en puerto 300011'));
